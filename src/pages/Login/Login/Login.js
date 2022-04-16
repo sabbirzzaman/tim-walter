0c38fcc36c5f5e8fe730e340e9008d7be1240490
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-17 01:56:50 +0600

    signup page added

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import TitleBanner from '../../Common/TitleBanner/TitleBanner';
-import './Login.css'
+import './Login.css';
 
 const Login = () => {
     return (
@@ -9,52 +9,57 @@ const Login = () => {
             <TitleBanner title="Login"></TitleBanner>
 
             <div className="form-container">
-            <div className="form-heading">
-                <h2>Login to your account</h2>
-            </div>
-            <form>
-                <div className="field-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Enter your email"
-                        id="email"
-                        required
-                    />
+                <div className="form-heading">
+                    <h2>Login to your account</h2>
                 </div>
-                <div className="field-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Enter your password"
-                        id="password"
-                        required
-                    />
+                <form>
+                    <div className="field-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            id="email"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            id="password"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <input type="submit" value="Login" />
+                    </div>
+
+                    <div className="field-group">
+                        <p>
+                            Don't have an account? <Link to="/signup">Create an account.</Link>
+                        </p>
+                    </div>
+                </form>
+
+                <div className="divider">
+                    <hr />
+                    <span>Or</span>
+                    <hr />
                 </div>
-                <div className="field-group">
-                    <input type="submit" value="Login" />
+
+                <div className="form-google">
+                    <button>Continue with Google</button>
                 </div>
-                <div className="field-group">
-                    <p>
-                        Don't have an account?{' '}
-                        <Link to="/signup">Create an account.</Link>
-                    </p>
+
+                <div className="form-facebook">
+                    <button>Continue with Facebook</button>
                 </div>
-            </form>
-            <div className="divider">
-                <hr />
-                <span>Or</span>
-                <hr />
             </div>
-            <div className="form-google">
-                <button>Continue with Google</button>
-            </div>
-            <div className="form-facebook">
-                <button>Continue with Facebook</button>
-            </div>
-        </div>
         </>
     );
 };
--- src/pages/Login/SignUp/SignUp.js
+++ src/pages/Login/SignUp/SignUp.js
@@ -1,10 +1,87 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import TitleBanner from '../../Common/TitleBanner/TitleBanner';
 
 const SignUp = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <TitleBanner title="Sign Up"></TitleBanner>
+
+            <div className="form-container">
+                <div className="form-heading">
+                    <h2>Create your account</h2>
+                </div>
+                <form>
+                    <div className="field-group">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="name"
+                            name="name"
+                            placeholder="Enter your name"
+                            id="name"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            id="email"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            id="password"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input
+                            type="password"
+                            name="confirm-password"
+                            placeholder="Confirm your password"
+                            id="confirm-password"
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <input type="submit" value="Sign Up" />
+                    </div>
+
+                    <div className="field-group">
+                        <p>
+                            Already have an account? <Link to="/login">Login</Link>
+                        </p>
+                    </div>
+                </form>
+
+                <div className="divider">
+                    <hr />
+                    <span>Or</span>
+                    <hr />
+                </div>
+
+                <div className="form-google">
+                    <button>Continue with Google</button>
+                </div>
+
+                <div className="form-facebook">
+                    <button>Continue with Facebook</button>
+                </div>
+            </div>
+        </>
     );
 };
 
