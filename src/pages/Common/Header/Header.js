37e74ef230212b2b60b03936ad581fd44a14fd7c
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-17 02:29:31 +0600

    active route added

--- src/pages/Common/Header/Header.js
+++ src/pages/Common/Header/Header.js
@@ -1,15 +1,31 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import logo from '../../../images/logo.svg';
 import './Header.css';
 
 const Header = () => {
     const navigate = useNavigate();
-
     const [user, loading] = useAuthState(auth);
+
+    function CustomLink({ children, to, ...props }) {
+        let resolved = useResolvedPath(to);
+        let match = useMatch({ path: resolved.pathname, end: true });
+
+        return (
+            <div>
+                <Link
+                    style={{ color: match ? '#cba557' : '' }}
+                    to={to}
+                    {...props}
+                >
+                    {children}
+                </Link>
+            </div>
+        );
+    }
 
     return (
         <div className="header-container">
@@ -22,9 +38,9 @@ const Header = () => {
                     />
                 </div>
                 <div className="navigation">
-                    <Link to="/home">Home</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/services">My Services</Link>
+                    <CustomLink to="/home">Home</CustomLink>
+                    <CustomLink to="/about">About Me</CustomLink>
+                    <CustomLink to="/services">My Services</CustomLink>
                 </div>
                 <div className="header-btn">
                     {user ? (
