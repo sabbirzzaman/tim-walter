d9d5ca9e47ce18e4555ab897927b9145d13a9188
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-16 23:18:35 +0600

    banner serction added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Banner.css'
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="banner-container">
+            <div className="overlay">
+                <div className="container">
+                    <h1>
+                        First-class lawyer and <br />
+                        consultant in USA.
+                    </h1>
+                    <p>
+                        I use innovative approaches to solve the <br />
+                        toughest challenges for businesses.
+                    </p>
+                    <button>My Services</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Banner></Banner>
+        </>
     );
 };
 
