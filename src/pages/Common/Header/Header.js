34da4c7d4923a5e10675efc2a4a12d816f9ccb61
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-17 02:22:46 +0600

    sign out button added

--- src/pages/Common/Header/Header.js
+++ src/pages/Common/Header/Header.js
@@ -1,24 +1,37 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
-import logo from '../../../images/logo.svg'
-import './Header.css'
+import auth from '../../../firebase.init';
+import logo from '../../../images/logo.svg';
+import './Header.css';
 
 const Header = () => {
     const navigate = useNavigate();
 
+    const [user, loading] = useAuthState(auth);
+
     return (
-        <div className='header-container'>
+        <div className="header-container">
             <nav className="container">
                 <div className="site-logo">
-                    <img onClick={() => navigate('/')} src={logo} alt="Site Logo" />
+                    <img
+                        onClick={() => navigate('/')}
+                        src={logo}
+                        alt="Site Logo"
+                    />
                 </div>
                 <div className="navigation">
-                    <Link to='/home'>Home</Link>
-                    <Link to='/about'>About Me</Link>
-                    <Link to='/services'>My Services</Link>
+                    <Link to="/home">Home</Link>
+                    <Link to="/about">About Me</Link>
+                    <Link to="/services">My Services</Link>
                 </div>
                 <div className="header-btn">
-                    <button onClick={() => navigate('login')}>Login</button>
+                    {user ? (
+                        <button onClick={() => signOut(auth)}>Sign Out</button>
+                    ) : (
+                        <button onClick={() => navigate('login')}>Login</button>
+                    )}
                 </div>
             </nav>
         </div>
